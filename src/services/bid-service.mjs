20ae91ae95cb9e9666e790f95fb11ab4f876b340
aahilalwani25 <alwani.aahil25@gmail.com>
2025-11-20 17:34:08 +0500

    chore: modified alreadyBiddedCache

--- src/services/bid-service.mjs
+++ src/services/bid-service.mjs
@@ -130,7 +130,7 @@ export const toggleAutoBidService = async ({ bidder_id }) => {
 
 export const autoBidService = async ({ clients, sub_user_doc_id, projectsToBid, bidderId, token, bidderName, general_proposal, autobid_proposal_type, autobid_type }) => {
     for (const project of projectsToBid) {
-        if ((autobid_type === AUTOBID_FOR_JOB_TYPE.ALL || (project.type === autobid_type)) && alreadyBiddedCache?.filter((val) => val === project?.id)?.length === 0) {
+        if ((autobid_type === AUTOBID_FOR_JOB_TYPE.ALL || (project.type === autobid_type)) && alreadyBiddedCache?.filter((val) => val.project_id === project?.id && val.sub_user_doc_id === sub_user_doc_id)?.length === 0) {
             try {
                 const bidAmount = calculateBidAmount(project);
                 // Skip projects that do not meet the criteria
@@ -203,7 +203,10 @@ export const autoBidService = async ({ clients, sub_user_doc_id, projectsToBid, 
                             notificationTitle: `Auto Bid Done from ${bidderName}`,
                             notificationDescription: `Project #${project.id} - ${project.title} has been Auto-Bidded from ${bidderName}`
                         });
-                        alreadyBiddedCache.push(project?.id);
+                        alreadyBiddedCache.push({
+                            sub_user_doc_id,
+                            project_id: project?.id
+                        });
                         break;
                     } else if (bidResponse?.status === 409) {
                         break;
